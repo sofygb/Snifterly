a9198b19e76cb37b42d7ff13187853b2863d526e
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-02 09:00:22 -0300

    a

--- nombre/App.js
+++ nombre/App.js
@@ -2,8 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let fecha = String(new Date().getMonth())
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Snifterly! {fecha}</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,4 +19,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontWeight: 'bold',
+    fontSize: '1.8rem',
+    display: block,
+  }
 });
