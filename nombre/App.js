cdf0d38580c54edce07db012d0574c5ba947d91b
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-09 08:54:05 -0300

    diseno

--- nombre/App.js
+++ nombre/App.js
@@ -1,30 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Button, Text, View, Alert, SafeAreaView} from 'react-native';
 
 export default function App() {
   let fecha = String(new Date().getMonth())
   return (
     <View style={styles.container}>
-      <View style={{flex: 1}}> <Text style={styles.title}>Snifterly!</Text> </View>
-      <View style={[{flex: 2}]}>
+      <View style={{flex: 1.2}}> <Text style={styles.title}>Snifterly!</Text> </View>
+
+      <View style={[{flex: 1.5}]}> 
 
         <View style={[styles.container,{flexDirection: 'row',},]}>
           <View style={styles.cuadro}>
-            <Text style={styles.subtitulo}>0 veces</Text> 
+            <Text style={styles.subtitulo}>CUADRO 1</Text> 
+            <Text style={styles.texto}>has ingresado mediciones</Text>
           </View>
           <View style={styles.cuadro}>
-            <Text style={styles.subtitulo}>0 mg/l</Text> 
+            <Text style={styles.subtitulo}>CUADRO 2</Text> 
           </View>
         </View>
-        
-        <View style={styles.cuadro}>
-          <Text style={styles.subtitulo}>0 veces</Text> 
+
+        <View style={[styles.container,{flexDirection: 'row',},]}>
+          <View style={styles.cuadro}>
+            <Text style={styles.subtitulo}>CUADRO 3</Text> 
+          </View>
+          <View style={styles.cuadro}>
+            <Text style={styles.subtitulo}>CUADRO 4</Text> 
+          </View>
         </View>
-        <View style={styles.cuadro}>
-          <Text style={styles.subtitulo}>0 mg/l</Text> 
-        </View>
-        </View>
+
+      </View>
       <View style={{flex: 2}}/>
+      <View style={styles.cuadro}>
+          <Text style={styles.subtitulo}>CUADRO 5</Text> 
+        </View>
       <View style={{flex: 1}}><View style={{flexDirection:'row', backgroundColor:'black'}}></View></View>
     </View>
   );
@@ -42,23 +50,47 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: 'bold',
     fontSize: '1.5rem',
-    marginTop: '1.5rem',
+    marginTop: '3rem',
   },
   subtitulo: {
     fontSize: '1.2rem',
-    margin: '1rem',
     marginLeft: '1rem',
+    marginRight: '1rem',
+    marginBottom: '0.2rem',
     flexDirection: 'row',
     justifyContent: 'center',
+    justifyContent: 'space-between',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#5654E1'
+  },
+  texto:{
+    marginLeft: '1rem',
+    marginRight: '1rem',
+    fontSize: '0.8rem',
+    color: '#4B4B4B',
+    fontFamily: 'inter',
   },
   cuadro: {
     flex: 0.3,
-    backgroundColor: 'white',
     borderWidth: 2,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     borderBottomRightRadius: 20,
     borderBottomLeftRadius: 20,
-    justifyContent: 'space-between',
+    padding: '0.5rem',
+    marginRight: '0.3rem',
+    marginLeft: '0.3rem',
+    minheight: '8rem',
+    minWidth: '10rem',
+    backgroundColor: '#ECECEC',
+    borderColor: '#ECECEC',
+    shadowColor: "#C4C4C4",
+        shadowOffset: {
+          width: 2,
+          height: 4,
+        },
+        shadowOpacity: 1,
+        shadowRadius: 5,
   },
 });
