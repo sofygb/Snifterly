3c1fa2e020a9d1e5016482dc635afc4f8f203b73
CamilaAylenLopez <camilalopezort@gmail.com>
2023-06-18 01:31:49 -0300

    Pantalla PrimeraHome

--- nombre/screens/PrimeraHome.js
+++ nombre/screens/PrimeraHome.js
@@ -6,20 +6,49 @@ import React, { useState, useEffect } from 'react';
 import * as Font from 'expo-font';
 
 export default function PrimeraHome({ navigation }) {
+    const loadJornada = async () => {
+        const data = await getJornada()
+        console.log(data)
+        setJornada([data])
+    }
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+    useEffect(() => {
+        if (!fontsLoaded) {
+            loadFonts();
+        }
+        loadJornada()
+    })
 
+    const loadFonts = async () => {
+        await Font.loadAsync({
+            'alata': require('../assets/fonts/Alata/Alata.ttf'),
+            'inter': require('../assets/fonts/Inter/Inter.ttf'),
+        });
+        setFontsLoaded(true);
+    }
     return (
         <View style={styles.container}>
+            <Text style={styles.titulo}>Snifterly</Text>
+
+            <View style={styles.botonAgregar}>
+                <TouchableOpacity onPress={() => { navigation.navigate('IngresoDeDatos') }}>
+                    <Icon icon="zondicons:add-solid" color="white" width={'10rem'} />
+                </TouchableOpacity>
+                <Text style={{ textAlign: 'center', color: 'white', fontSize: '1.5rem', fontFamily: 'Alata' }}>nueva jornada</Text>
+            </View>
+
             <View style={styles.footer}>
                 <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                     <TouchableOpacity onPress={() => { navigation.navigate('PrimeraHome') }}>
-                        <Icon icon="material-symbols:home" width={'2.5rem'} />
+                        <Icon icon="material-symbols:home" width={'2.5rem'} color="white" />
                     </TouchableOpacity>
                     <TouchableOpacity onPress={() => { navigation.navigate('Historial') }}>
-                        <Icon icon="zondicons:calendar" width={'2.3rem'} />
+                        <Icon icon="zondicons:calendar" width={'2.3rem'} color="white" />
                     </TouchableOpacity>
-                    <Icon icon="mdi:account" width={'2.5rem'} />
+                    <Icon icon="mdi:account" width={'2.5rem'} color="white" />
                 </View>
             </View>
+
         </View>
     )
 }
@@ -30,14 +59,32 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: 'orange',
+        fontFamily: 'alata',
     },
-    footer:{
+    titulo: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontWeight: 'bold',
+        fontSize: '2rem',
+        marginTop: '3rem',
+        marginBottom: '8rem',
+        fontFamily: 'alata',
+        color: 'white',
+    },
+    footer: {
         flex: 1,
         display: 'flex',
         justifyContent: 'flex-end',
-        marginBottom: '1rem', 
+        marginBottom: '1rem',
         width: '100%',
         paddingLeft: '2rem',
         paddingRight: '2rem',
     },
+    botonAgregar: {
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'center',
+    }
 });
--- nombre/screens/SalirJornada.js
+++ nombre/screens/SalirJornada.js
@@ -1,26 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, Text, View, Alert, SafeAreaView, TouchableOpacity, InputAccessoryView, ScrollView} from 'react-native';
+import { StyleSheet, Button, Text, View, Alert, SafeAreaView, TouchableOpacity, InputAccessoryView, ScrollView } from 'react-native';
 import { TextInput } from "@react-native-material/core";
 import { Icon } from '@iconify/react';
 import React, { useState, useEffect } from 'react';
 import * as Font from 'expo-font';
 
 export default function SalirJornada({ navigation }) {
+  const loadJornada = async () => {
+    const data = await getJornada()
+    console.log(data)
+    setJornada([data])
+  }
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  useEffect(() => {
+    if (!fontsLoaded) {
+      loadFonts();
+    }
+    loadJornada()
+  })
 
-  return(
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      'alata': require('../assets/fonts/Alata/Alata.ttf'),
+      'inter': require('../assets/fonts/Inter/Inter.ttf'),
+    });
+    setFontsLoaded(true);
+  }
+  return (
     <View style={styles.container}>
-      <View style={{flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'flex-end'}}>
+      <View style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
         <Text style={styles.textoPrincipal}>¿Desea seguir con la jornada?</Text>
       </View>
-            
-        <View style={[styles.espaciosBotones, {flez: 1, flexDirection: 'row', display: 'flex', alignItems: 'flex-start'}]}>
-            <TouchableOpacity style={styles.botonAceptar} onPress = { () => {navigation.navigate('Home')}}>
-                <Text style={[{color: 'white', fontSize: '1rem', fontFamily: 'inter'}]}>Seguir</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.botonCancelar} onPress = { () => {navigation.navigate('PrimeraHome')}}>
-                <Text style={[{color: 'red', fontSize: '1rem', fontFamily: 'inter'}]}>Finalizar</Text>
-            </TouchableOpacity>
-        </View>
+
+      <View style={[styles.espaciosBotones, { flez: 1, flexDirection: 'row', display: 'flex', alignItems: 'flex-start' }]}>
+        <TouchableOpacity style={styles.botonAceptar} onPress={() => { navigation.navigate('Home') }}>
+          <Text style={[{ color: 'white', fontSize: '1rem', fontFamily: 'inter' }]}>Seguir</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botonCancelar} onPress={() => { navigation.navigate('PrimeraHome') }}>
+          <Text style={[{ color: 'red', fontSize: '1rem', fontFamily: 'inter' }]}>Finalizar</Text>
+        </TouchableOpacity>
+      </View>
 
     </View>
   )
@@ -28,47 +47,47 @@ export default function SalirJornada({ navigation }) {
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    botonAceptar: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      minWidth: '8rem',
-      backgroundColor:"#5654E1",
-      borderRadius: 15,
-      padding: 10,
-    },
-    botonCancelar: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      minWidth: '8rem',
-      backgroundColor:"#D9D9D9", 
-      borderRadius: 15,
-      padding: 10,
-    },
-    espaciosBotones:{
-      flex: 1,
-      display: 'flex',
-      width: '100%',
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      marginTop: '2rem',
-      paddingLeft: '2rem',
-      paddingRight: '2rem', 
-    },
-    textoPrincipal:{
-      display: 'flex',
-      fontSize: '1.5rem',
-      marginTop: '3rem',
-      fontFamily: 'alata',
-      marginLeft: '2rem',
-      marginRight: '2rem',
-      justifyContent: 'center',
-      textAlign: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  botonAceptar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minWidth: '8rem',
+    backgroundColor: "#5654E1",
+    borderRadius: 15,
+    padding: 10,
+  },
+  botonCancelar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minWidth: '8rem',
+    backgroundColor: "#D9D9D9",
+    borderRadius: 15,
+    padding: 10,
+  },
+  espaciosBotones: {
+    flex: 1,
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: '2rem',
+    paddingLeft: '2rem',
+    paddingRight: '2rem',
+  },
+  textoPrincipal: {
+    display: 'flex',
+    fontSize: '1.5rem',
+    marginTop: '3rem',
+    fontFamily: 'alata',
+    marginLeft: '2rem',
+    marginRight: '2rem',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+});
