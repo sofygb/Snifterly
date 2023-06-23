17f728caba1dd4ec99ae320addaf0793c7b37b9a
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-23 11:29:21 -0300

    front terminado

--- frontend/api.js
+++ frontend/api.js
@@ -3,6 +3,9 @@
 const API = 'http://localhost:3000/jornadas/count/3'
 
 export const getJornada = async () => {
-    const res = await fetch(API)
+    const res = await fetch(API, {
+        METHOD: "GET",
+    })
+    console.log(res) 
     return await res.json()
 }
